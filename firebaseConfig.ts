29ac3e1ec7388ab0802c556945c21e007d5d4abd
pablocolmenarejo-dev <pablo.colmenarejo@meisys.es>
2025-08-04 12:11:42 +0200

    Create firebaseConfig.ts

---
+++ firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB3intXLhGGx0CVXg_v1IxnPpKQw66kn6c",
+  authDomain: "porramusical.firebaseapp.com",
+  databaseURL: "https://porramusical-default-rtdb.firebaseio.com",
+  projectId: "porramusical",
+  storageBucket: "porramusical.firebasestorage.app",
+  messagingSenderId: "509551778698",
+  appId: "1:509551778698:web:54e67472a31eab91524673"
+};
